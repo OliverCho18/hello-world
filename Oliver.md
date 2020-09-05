a8e7447592fe9a980b715cd0439d98d7590ca17c
Oliver Cho <olivercho@Olivers-MacBook-Pro.local>
2020-09-05 16:35:40 -0700

    First Part with everything

---
+++ Oliver.md
@@ -0,0 +1,2 @@
+Today I could not find the lego sets I wanted.
+but I will try another lego store tomorrow
--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ class helloworld{
   public static void main(String [] args)
   {
     System.out.println("Hello, my name is Oliver");
+    System.out.println("Hello my name is Tom");
   }
 }

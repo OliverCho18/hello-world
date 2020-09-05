d5972ecf1dbceb6b0840cd3d518214cdca4b500e
Oliver Cho <olivercho@Olivers-MacBook-Pro.local>
2020-09-05 16:56:33 -0700

    Added print line regarding bucks

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,6 @@ class helloworld{
   {
     System.out.println("Hello, my name is Oliver");
     System.out.println("Hello my name is Tom");
+    System.out.println("People criticize me for supporing the bucks");
   }
 }
